7face4bef4917c7391a05dcdb9dc32a67caf6748
Krys Frost <librarykrystal@gmail.com>
2022-11-17 23:28:42 -0600

    Text fields, button, and table bones added to HTML. JS onReady function added. CSS file added.

--- project/script.js
+++ project/script.js
@@ -1,1 +1,18 @@
 console.log('JS file TEST');
+
+$(document).ready(onReady);
+
+function onReady(){
+    console.log('f onReady TEST');
+    $('#addEmployee').on('click', addEmployee)
+}
+
+function addEmployee(){
+    console.log('f addEmployee TEST');
+}
+
+
+
+
+// going to need to... append to a table instead of a ul?
+// so use backticks and write out <td> instead of <li>?
