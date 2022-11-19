404e4b38599d72a44e8f174395499d4acc2db8b9
Krys Frost <librarykrystal@gmail.com>
2022-11-19 15:03:16 -0600

    addEmpoyee function contains object constructor, pushes to array, and clears text fields.

--- project/script.js
+++ project/script.js
@@ -4,14 +4,34 @@ $(document).ready(onReady);
 
 function onReady(){
     console.log('f onReady TEST');
-    $('#addEmployee').on('click', addEmployee)
+    $('#submitBtn').on('click', addEmployee)
 }
+
+let employeesList = [];
 
 function addEmployee(){
     console.log('f addEmployee TEST');
+    let employeeInput = {
+        first: $('#firstInput').val(),
+        last: $('#lastInput').val(),
+        idNum: $('#idNumInput').val(),
+        jobTitle: $('#jobTitleInput').val(),
+        salary: $('#salaryInput').val(),
+    }
+    employeesList.push(employeeInput);
+    console.log(employeesList);
+    // call next (render to DOM) function here
+    $('#firstInput').val('');
+    $('#lastInput').val('');
+    $('#idNumInput').val('');
+    $('#jobTitleInput').val('');
+    $('#salaryInput').val('');
 }
 
-
+function monthlyCostCalc (){
+    // Grab salary key from each object in main array
+    // add salaries to their own array?
+}
 
 
 // going to need to... append to a table instead of a ul?
